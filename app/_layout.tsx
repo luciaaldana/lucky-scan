4993c60954ede3a9c70569d9d1ca2193f0a68d63
luciaaldana <luciaaldanacastillo@gmail.com>
2024-12-06 19:58:39 -0300

    feat(layout): add header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,16 @@
 import { useEffect } from 'react';
-import { Image } from 'react-native';
-import { Stack } from 'expo-router';
+import { Image, Pressable } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useFonts } from 'expo-font';
+import Entypo from '@expo/vector-icons/Entypo';
 import 'react-native-reanimated';
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const router = useRouter();
+
   const [loaded] = useFonts({
     NunitoSans: require('../assets/fonts/NunitoSans.ttf'),
   });
@@ -32,7 +35,24 @@ export default function RootLayout() {
       }}
     >
       <Stack.Screen name="index" options={{ headerShown: true }} />
-      <Stack.Screen name="photo" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="photo"
+        options={{
+          headerShown: true,
+          headerLeft: () => {
+            const handleDismiss = () => {
+              if (router.canDismiss()) {
+                router.dismiss(1);
+              }
+            };
+            return (
+              <Pressable onPress={handleDismiss}>
+                <Entypo name="chevron-left" size={24} color="#4dba7a" />
+              </Pressable>
+            );
+          },
+        }}
+      />
     </Stack>
   );
 }
